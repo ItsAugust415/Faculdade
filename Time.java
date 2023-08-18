135e56f590644822b64df9a235c8f1ace0342fea
ItsAugust415 <90720407+ItsAugust415@users.noreply.github.com>
2023-08-18 19:47:30 -0400

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,34 @@
+public class Time {
+    private String corDaCamisa, cidade, tamanhoTorcida, popularidade;
+    
+    public String getCorDaCamisa(){
+        return corDaCamisa;
+    }
+    public void setCorDaCamisa(String corDaCamisa){
+        this.corDaCamisa = corDaCamisa;
+    }
+
+    public String getCidade(){
+        return cidade;
+    }
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+
+    public String getTamanhoTorcida(){
+        return tamanhoTorcida;
+    }
+    public void setTamanhoTorcida(){
+        this.tamanhoTorcida = tamanhoTorcida;
+    }
+
+    public String getPopularidade(){
+        return popularidade;
+    }
+    public void setPopularidade(){
+        this.popularidade = popularidade;
+    }
+
+
+
+}
